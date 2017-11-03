4b5aeacdc0bc0a23d33fd94c209c802b02933cee
MarkusZhang <zhangjie.iet@gmail.com>
2017-11-03 08:50:53 +0800

    add create dataset split function

--- file_organize.py
+++ file_organize.py
@@ -4,6 +4,12 @@ Utilities for organizing files
 import os, random
 from shutil import copyfile
 
+def _block_copy(source_dir,filenames,target_dir):
+    "copy `filenames` from source_dir to target_dir"
+    for file in filenames:
+        source_file = os.path.join(source_dir, file)
+        tgt_file = os.path.join(target_dir, file)
+        copyfile(source_file, tgt_file)
 
 def generate_label_folders(source_dir="mnist_m_test", target_dir="test", mapping_file="mnist_m_test_labels.txt"):
     "this will move files from `source_dir` to subdirectories in `target_dir` according to `mapping_file`"
@@ -39,3 +45,24 @@ def create_mini_dataset(source_dir, target_dir, sample_rate):
             tgt_file = os.path.join(tgt_subdir, file)
             copyfile(source_file, tgt_file)
 
+def create_dataset_split(source_dir,target_dirs,split_ratio):
+    """
+    :param source_dir: this folder should contain class labels as sub-directoriy names
+    :param target_dirs: a list of two dir names, where the files from `source_dir` will be copied in
+    :param split_ratio: 0~1
+    """
+    target_A, target_B = target_dirs
+    labels = os.listdir(source_dir)
+    for label in labels:
+        subdir = os.path.join(source_dir, label)
+        files = os.listdir(subdir)
+        files_to_A = random.sample(files, int(split_ratio * len(files)))
+        files_to_B = [f for f in files if f not in files_to_A]
+        # move to `files_to_A` to target_A
+        tgt_A_subdir = os.path.join(target_A, label)
+        if (not os.path.exists(tgt_A_subdir)): os.makedirs(tgt_A_subdir)
+        _block_copy(source_dir=subdir,target_dir=tgt_A_subdir,filenames=files_to_A)
+        # move to `files_to_B` to target_B
+        tgt_B_subdir = os.path.join(target_B, label)
+        if (not os.path.exists(tgt_B_subdir)): os.makedirs(tgt_B_subdir)
+        _block_copy(source_dir=subdir, target_dir=tgt_B_subdir, filenames=files_to_B)
