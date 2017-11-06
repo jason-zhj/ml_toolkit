f9d54411022b85b48600224e7fe1caca5320ec32
MarkusZhang <zhangjie.iet@gmail.com>
2017-11-06 16:57:30 +0800

    add calculate_precision_recall
    this function returns precision, recall at all hamming distance, radius

--- hash_toolkit/metrics.py
+++ hash_toolkit/metrics.py
@@ -2,7 +2,7 @@
 this includes the computation for precision, recall, MAP
 """
 #TODO: add MAP measure
-import itertools
+import itertools, operator
 import io
 import numpy as np
 from PIL import Image
@@ -86,33 +86,75 @@ def _get_hdist(code1, code2):
     return len([1 for i in range(len(code1)) if code1[i]!=code2[i]])
 
 
-def calculate_precision(radius,db_set,test_set):
+def _calc_precision_recall(radius,item,db_set):
     """
     :param radius:
-    :param db_set: the data acting as the database, list of dict {"label":..,"hash":""}
-    :param test_set: list of dict {"label":..,"hash":""}
-    :return: the averaged precision over `test_set`
+    :param item: {"hash":...,"label":..}
+    :param db_set:
+    :return: precisions, recalls, MAP for each distance within `radius`
     """
-    avg_precision = 0
-    for item in test_set:
-        query_hash = item["hash"]
-        query_label = item["label"]
-        # retrieve items from `db_set`
-        hashes_within_radius = _compute_hash_within_radius(query_hash, radius)
+    query_hash = item["hash"]
+    query_label = item["label"]
+    total_correct = sum([1 for item in db_set if item["label"]==query_label])  # total number of correct items to be retrieved
+    correct_retrieved = [0 for i in range(radius+1)]  # record # correctly retrieved for each distance
+    total_retrieved = [0 for i in range(radius+1)] # record # retrieved for each distance
+    # record correct_retrieved and total_retrieved for each distance within radius
+    for dist in range(radius+1):
+        hashes_to_retr = _compute_hash_with_dist(hashcode=query_hash, dist=dist)
         retrieved_items = []
-        for hashcode in hashes_within_radius:
+        for hashcode in hashes_to_retr:
             retrieved_items += _retrieve_items_using_hash(db_set=db_set, hashcode=hashcode)
 
-        # calculate precision for this test item
-        num_correctly_retrieved = len([item for item in retrieved_items if item["label"]==query_label])
-        if (len(retrieved_items) > 0 ):
-            precision = num_correctly_retrieved * 1.0 / len(retrieved_items)
-        else:
-            precision = 0
+        total_retrieved[dist] = len(retrieved_items)
+        correct_retrieved[dist] = len([item for item in retrieved_items if item["label"] == query_label])
 
-        avg_precision += precision / len(test_set)
+    # calc precision recall
+    dist_precisions = [correct_retrieved[i] * 1.0 / total_retrieved[i] if total_retrieved[i] > 0 else 0
+                       for i in range(len(total_retrieved))]
+    radius_precisions = [sum(correct_retrieved[:i+1]) * 1.0 / sum(total_retrieved[:i+1]) if total_retrieved[i] > 0 else 0
+                       for i in range(len(total_retrieved))]
+    dist_recalls = [correct_retrieved[i] * 1.0 / total_correct if total_correct > 0 else 0
+                       for i in range(len(correct_retrieved))]
+    radius_recalls = [sum(correct_retrieved[:i+1]) * 1.0 / total_correct if total_correct > 0 else 0
+                       for i in range(len(correct_retrieved))]
+    # calc MAP
+    index_to_use = [i for i in range(len(correct_retrieved)) if correct_retrieved[i] > 0]
+    avg_precision = sum([radius_precisions[i] for i in index_to_use]) / len(index_to_use) \
+        if len(index_to_use) > 0 else 0
+    return {
+        "precision-dist": dist_precisions,
+        "precision-radius": radius_precisions,
+        "recall-dist": dist_recalls,
+        "recall-radius": radius_recalls,
+        "avg-precision": avg_precision
+    }
 
-    return avg_precision
+def calculate_precision_recall(radius, db_set, test_set):
+    """
+    :param dist_or_radius: either dist or radius
+    :param use_dist: if True, `dist_or_radius` will be used as distance, otherwise as radius
+    :return: result_dict, see below
+    """
+    assert radius >=0
+    result_dict = {
+        "precision-dist":[0 for _ in range(radius+1)],
+        "precision-radius":[0 for _ in range(radius+1)],
+        "recall-dist":[0 for _ in range(radius+1)],
+        "recall-radius":[0 for _ in range(radius+1)],
+    }
+    mean_avg_precision = 0
+    for item in test_set:
+        item_result = _calc_precision_recall(radius=radius,item=item, db_set=db_set)
+        for key in result_dict.keys():
+            result_dict[key] = list(map(operator.add,result_dict[key],item_result[key]))
+        mean_avg_precision += item_result["avg-precision"]
+
+    # divide by number of test items
+    for key in result_dict.keys():
+        result_dict[key] = [item / len(test_set) for item in result_dict[key]]
+    result_dict["mean-avg-precision"] = mean_avg_precision / len(test_set)
+
+    return result_dict
 
 
 def get_mean_avg_precision(test_set,db_set,maxdist):
@@ -166,7 +208,7 @@ def get_precision_vs_recall(test_set,db_set,max_hdist):
     :param test_set: list of dict {"label":..,"hash":""}
     :param db_set: the data acting as the database
     :param max_hdist: maximum hamming distance two hash code can have
-    :return: a list of {"precisions":[precision@dist0,precision@dist1,...],"recalls":[recall@dist0,recall@dist1...]}
+    :return: a list of {"precisions":[precision@r=0,precision@r=1,...],"recalls":[recall@r=0,recall@r=1...]}
     """
     results = []
     # for each test item, there is a precision list and recall list
