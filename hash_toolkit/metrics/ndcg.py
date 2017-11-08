9775805e90e23bb879f0ae9a535c3231d4f22427
MarkusZhang <zhangjie.iet@gmail.com>
2017-11-08 15:28:50 +0800

    two changes
    1. restructure hash_toolkit
    2. add NDCG measure for single-labeled data

---
+++ hash_toolkit/metrics/ndcg.py
@@ -0,0 +1,69 @@
+import math
+from hash_toolkit.metrics.utils import _compute_hash_with_dist, _retrieve_items_using_hash
+
+#TODO: need more comprehensive testing
+
+def _calc_ndcg_from_rel_ls(rel_ls):
+    "apply NDCG formula to relevancy list"
+    return sum(
+        [(2**rel_ls[i]-1)/math.log(2+i) for i in range(len(rel_ls))]
+    )
+
+def _calc_ndcg_with_topk(item,db_set,k,rel_score):
+    "calculate NDCG for first `k` retrieved items"
+    assert rel_score > 0
+
+    query_hash = item["hash"]
+    query_label = item["label"]
+    retr_rel_ls = []
+    for dist in range(len(query_hash)):
+        hashes_to_retr = _compute_hash_with_dist(hashcode=query_hash, dist=dist)
+        retrieved_items = []
+        for hashcode in hashes_to_retr:
+            retrieved_items += _retrieve_items_using_hash(db_set=db_set, hashcode=hashcode)
+        # relevancy score, sorted from low to high
+        rel_ls = sorted([rel_score if i["label"]==query_label else 0 for i in retrieved_items])
+        if (len(retr_rel_ls) + len(rel_ls) < k):
+            retr_rel_ls += rel_ls
+        else:
+            retr_rel_ls += rel_ls[:k-len(retr_rel_ls)]
+
+    return _calc_ndcg_from_rel_ls(rel_ls=retr_rel_ls)
+
+def _calc_ndcg_with_radius(item,db_set,radius,rel_score):
+    "calculate NDCG for items within `radius`"
+    query_hash = item["hash"]
+    query_label = item["label"]
+    retr_rel_ls = []
+    for dist in range(radius + 1):
+        hashes_to_retr = _compute_hash_with_dist(hashcode=query_hash, dist=dist)
+        retrieved_items = []
+        for hashcode in hashes_to_retr:
+            retrieved_items += _retrieve_items_using_hash(db_set=db_set, hashcode=hashcode)
+        # relevancy score, sorted from low to high
+        rel_ls = sorted([rel_score if i["label"] == query_label else 0 for i in retrieved_items])
+        retr_rel_ls += rel_ls
+
+    return _calc_ndcg_from_rel_ls(rel_ls=retr_rel_ls)
+
+def _calc_ndcg(item,db_set,radius_or_topk,use_topk,rel_score=2):
+    """
+    Implementation of Normalized Discounted Cumulative Gain
+    Here we consider the worst case, for the db items with the same hamming distance, we assume wrong items are ranked higher
+    :param radius_or_topk:
+    :param use_topk: if this is True,  `radius_or_topk` will be used as K
+    :param rel_score: relevancy score for correct item
+    """
+
+    if (use_topk):
+        K = radius_or_topk
+        return _calc_ndcg_with_topk(item=item,db_set=db_set,k=K,rel_score=rel_score)
+    else:
+        R = radius_or_topk
+        return _calc_ndcg_with_radius(item=item,db_set=db_set,radius=R,rel_score=rel_score)
+
+def calculate_NDCG(query_set,db_set,radius_or_topk,use_topk,rel_score=2):
+    "return the average NDCG for items in `query_set`, refer to `_calc_ndcg` for the use of parameters"
+    return sum(
+        [_calc_ndcg(item=item,db_set=db_set,radius_or_topk=radius_or_topk,use_topk=use_topk,rel_score=rel_score) for item in query_set]
+    ) / len(query_set)
--- hash_toolkit/metrics/precision_recall.py
+++ hash_toolkit/metrics/precision_recall.py
@@ -2,88 +2,14 @@
 this includes the computation for precision, recall, MAP
 """
 #TODO: add MAP measure
-import itertools, operator
-import io
-import numpy as np
-from PIL import Image
+# TODO: add MAP measure
+
+# TODO: add MAP measure
+# TODO: add MAP measure
+import operator
+
 import matplotlib.pyplot as plt
-
-def _pyplot_to_image(plt_obj):
-    "return a PIL.Image object"
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
-    im = Image.open(buf)
-    return im
-
-
-def _fig2data(fig):
-    """
-    @brief Convert a Matplotlib figure to a 4D numpy array with RGBA channels and return it
-    @param fig a matplotlib figure
-    @return a numpy 3D array of RGBA values
-    """
-    # draw the renderer
-    fig.canvas.draw()
-    # Get the RGBA buffer from the figure
-    w, h = fig.canvas.get_width_height()
-    buf = np.fromstring(fig.canvas.tostring_argb(), dtype=np.uint8)
-    buf.shape = (w, h, 4)
-    # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
-    buf = np.roll(buf, 3, axis=2)
-    return buf
-
-
-def _fig2img (fig):
-    """
-    @brief Convert a Matplotlib figure to a PIL Image in RGBA format and return it
-    @param fig a matplotlib figure
-    @return a Python Imaging Library ( PIL ) image
-    """
-    # put the figure pixmap into a numpy array
-    buf = _fig2data (fig)
-    w, h, d = buf.shape
-    return Image.frombytes( "RGBA", ( w ,h ), buf.tostring( ) )
-
-def process_hash_csv(file_path,delimiter=","):
-    "return list of {'label':..,'hash':'010101'}"
-    results = []
-    with open(file_path,"r") as f:
-        lines = f.readlines()[1:]
-        for line in lines:
-            filename,label,hashcode = line.strip().split(delimiter)
-            hashcode = hashcode.replace(" ","")
-            results.append({"label":label,"hash":hashcode})
-    return results
-
-def _compute_hash_with_dist(hashcode, dist):
-    "`hashcode` should only contain '0' and '1', return a list of hash strings, whose hamming distance to `hashcode` is `dist`"
-    hash_ls = []
-    invert = lambda x:"1" if x=="0" else "0"
-    code_length = len(hashcode)
-    for positions in itertools.combinations(range(code_length),dist):
-        # invert the hash bit at `positions`
-        computed_code = "".join([invert(hashcode[i]) if i in positions else hashcode[i]
-                                 for i in range(code_length)])
-        hash_ls.append(computed_code)
-
-    return hash_ls
-
-def _compute_hash_within_radius(hashcode, radius):
-    "return list of hash strings, `hashcode` should be binary string '0101..'"
-    hash_ls = []
-    for i in range(radius+1):
-        hash_ls += _compute_hash_with_dist(hashcode=hashcode, dist=i)
-    return hash_ls
-
-def _retrieve_items_using_hash(db_set, hashcode):
-    "return a list of {'label':..,'hash':..}, retrieved from `db_set`, `db_set` should be list of dict"
-    return [item for item in db_set if item["hash"]==hashcode]
-
-def _get_hdist(code1, code2):
-    "return hamming distance"
-    assert len(code1) == len(code2)
-    return len([1 for i in range(len(code1)) if code1[i]!=code2[i]])
+from hash_toolkit.metrics.utils import _fig2img, _compute_hash_with_dist, _retrieve_items_using_hash, _get_hdist
 
 
 def _calc_precision_recall(radius,item,db_set):
@@ -185,11 +111,11 @@ def _get_avg_precision_for_query(query, db_set, maxdist):
     total_retrieved = 0
     correct_retrieved = 0
     for d in range(maxdist):
-        hash_ls = _compute_hash_with_dist(hashcode=query_code,dist=d)
+        hash_ls = _compute_hash_with_dist(hashcode=query_code, dist=d)
         retrieved_items = []
         include = False # include precision at this distance when there is correct item retrieved
         for hash in hash_ls:
-            retrieved_items = _retrieve_items_using_hash(db_set=db_set,hashcode=hash)
+            retrieved_items = _retrieve_items_using_hash(db_set=db_set, hashcode=hash)
             total_retrieved += len(retrieved_items)
             new_correct = len([t for t in retrieved_items if t["label"]==query_label])
             if (new_correct > 0):
